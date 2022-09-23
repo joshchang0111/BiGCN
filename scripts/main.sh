522b23c173fec42c1b28caf459fdbad128928f2a
joshchang0111 <j5521631@gmail.com>
2022-09-23 16:09:55 +0800

    Modify script

--- scripts/main.sh
+++ scripts/main.sh
@@ -16,9 +16,22 @@
 #python ./model/Twitter/BiGCN_Twitter.py --dataset_name Twitter15 --flatten
 #python ./model/Twitter/BiGCN_Twitter.py --dataset_name Twitter16 --flatten
 
+if [ $(hostname) = "josh-System-Product-Name" ]; then
+	data_root="/mnt/hdd1/projects/BiGCN/dataset/processedV2"
+	output_root="/mnt/hdd1/projects/BiGCN"
+elif [ $(hostname) = "yisyuan-PC2" ]; then
+	export CUDA_VISIBLE_DEVICES=0
+	data_root="/home/joshchang/project/BiGCN/data/processedV2"
+	output_root="/home/joshchang/project/BiGCN/results"
+fi
+#else
+#	#export CUDA_VISIBLE_DEVICES=0
+#	output_root="/nfs/home/joshchang/projects/RumorV2/results"
+#fi
+
 #########################
 ## Train on my dataset ##
 #########################
-python ./model/BiGCN.py --dataset_name twitter15
-python ./model/BiGCN.py --dataset_name twitter16
-python ./model/BiGCN.py --dataset_name semeval2019
+python ./model/BiGCN.py --dataset_name twitter15 --data_root "$data_root" --output_root "$output_root"
+python ./model/BiGCN.py --dataset_name twitter16 --data_root "$data_root" --output_root "$output_root"
+python ./model/BiGCN.py --dataset_name semeval2019 --data_root "$data_root" --output_root "$output_root"
